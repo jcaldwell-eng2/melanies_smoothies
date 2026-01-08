508ca8812066ceb0093aaab36d7d44589841e1aa
jcaldwell-eng2 <caldwell.1972@yahoo.com>
2026-01-08 12:30:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list:
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success('Your Smoothie is ordered '+ ' , '+name_on_order+ ' ')
 
-mport requests
-moothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
 
